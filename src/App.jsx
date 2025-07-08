d27160e51fc213078f29984844f8cc0655be4564
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-09 00:02:27 +0500

    Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import PageNotFound from './PageNotFound';
 
 function App() {
   
@@ -14,6 +15,9 @@ function App() {
     <Routes>
      <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      {/* Page not found */}
+      {/* <Route path="/*" element={<h1>404 Not Found</h1>} /> */}
+      <Route path="/*" element={<PageNotFound/>} />
     </Routes>
    </div>
   );
