6bffeb5db7b5bb780d3a6ee454e8d79c4dd730ad
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-07-08 23:04:05 +0500

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import { useState } from 'react';
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <Router>
-      <nav>
-        <Link to="/home">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+   <div>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    
+    <Routes>
+     <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+   </div>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      <p>Welcome to the home page!</p>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-      <p>This is the about page.</p>
-    </div>
-  );
-}
 
 export default App;
